f944c3172a735f5e8f06f1093514a31bca9df322
AbhishekPrajapati7508 <mr.prajapati90232@gmail.com>
2025-11-10 23:38:38 +0530

    Added HelloWorld program

---
+++ forloop.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class forloop {
+    
+    public static void main(String[]args){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the value of a: ");
+        int a= sc.nextInt();
+        System.out.print("Enter the  value of b : ");
+        int b=sc.nextInt();
+        for(int i=a;i<=b;i++){
+            System.out.print(i+" ");
+
+        }
+
+    }
+}
